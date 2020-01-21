c7c37e51a6331140cf5e96b1f929531d53113505
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2020-01-21 03:08:45 -0600

    el primer comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from './components/home/home.component';
+import { WhoiamComponent } from './components/whoiam/whoiam.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
+
+
+
+
 @NgModule({
+
   declarations: [
-    AppComponent
+    //se declaran los componentes creados por el desarrollador
+    AppComponent,
+
+    NavbarComponent,
+    HomeComponent,
+    WhoiamComponent,
+    TechnologiesComponent,
+
   ],
   imports: [
+    //se  cargan los modulos de la aplicacion
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    //aqui se cargan los servicios 
+  ],
+
+  //es el primer componente el cual se abrira el modulo
   bootstrap: [AppComponent]
 })
 export class AppModule { }
